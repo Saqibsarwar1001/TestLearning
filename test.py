e4fb29434a481c0bc6683da5ea38ee4311fff04d
Saqib Sarwar <saqibsarwar1001@gmail.com>
2023-01-15 18:01:58 +0500

    Added dQBC.doc dBC.docx index.html python.py python.py1 python2.py test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+# first=int(input("Enter your first "))
+# second=int(input("Enter your second "))
+# total = first + second
+
+
+# print(total)
+
+# name,age,city= "Ali",20,"KHI"
+# print(name, age,city )
+
+
+# name, age=input("Enter your name and age ").split("-")
+# print(name, age )
+
+
+# print('i\'m Saqib')
+
+# print("\tFirst Statement Second Statement")
+
+
+# math,physics,stat = input("Enter marks of Math, physics, Stat ").split()
+# print(type(math),type(physics),type(stat))
+# math,physics,stat=float(math),float(physics),float(stat)
+# print(type(math),type(physics),type(stat))                            
+# sum=math+physics+stat
+# print(sum)
+
+print(bool(0))
